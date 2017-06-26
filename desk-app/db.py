ab5206bd8212d449cde4785d0bf7db516715f27a
Rahul Patil <rahulpatil61197@gmail.com>
2017-06-26 16:08:38 +0530

    Code for using POSTgre SQL as backend

--- desk-app/db.py
+++ desk-app/db.py
@@ -1,22 +1,21 @@
-import sqlite3 as sql
-import pyscopg2
+import psycopg2 as psy
 
 def create_table():
-    conn = sql.connect("app_db.db")
+    conn = psy.connect("dbname = 'database1' user = 'postgres' password = 'post123' host = 'localhost' port = '5432'")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item, quant, price):
-    conn = sql.connect("app_db.db")
+    conn = psy.connect("dbname = 'database1' user = 'postgres' password = 'post123' host = 'localhost' port = '5432'")
     cur = conn.cursor()
-    cur.execute("INSERT INTO store VALUES (?, ?, ?)",(item, quant, price))
+    cur.execute("INSERT INTO store VALUES (%s, %s, %s)", (item, quant, price))              #to prevent SQL injections
     conn.commit()
     conn.close()
 
 def view():
-    conn = sql.connect("app_db.db")
+    conn = psy.connect("dbname = 'database1' user = 'postgres' password = 'post123' host = 'localhost' port = '5432'")
     cur = conn.cursor()
     cur.execute("SELECT * from store")
     data = cur.fetchall()
@@ -24,17 +23,19 @@ def view():
     return data
 
 def delete(item):
-    conn = sql.connect("app_db.db")
+    conn = psy.connect("dbname = 'database1' user = 'postgres' password = 'post123' host = 'localhost' port = '5432'")
     cur = conn.cursor()
-    cur.execute("DELETE FROM store where item = ?", (item,))
+    cur.execute("DELETE FROM store where item = %s", (item,))
     conn.commit()
     conn.close()
 
 def update(quant, price, item):
-    conn = sql.connect("app_db.db")
+    conn = psy.connect("dbname = 'database1' user = 'postgres' password = 'post123' host = 'localhost' port = '5432'")
     cur = conn.cursor()
-    cur.execute("UPDATE store SET quantity = ?, price = ? WHERE item = ?", (quant, price, item))
+    cur.execute("UPDATE store SET quantity = %s, price = %s WHERE item = %s", (quant, price, item))
     conn.commit()
     conn.close()
 
+
 create_table()
+print(view())
